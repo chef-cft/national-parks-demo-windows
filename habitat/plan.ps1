834953b99b847eb76e272ddbc49e8ba01b222f12
Jeff Brimager <JBrimager@gmail.com>
2019-06-19 12:22:09 -0500

    working mostly db config is bad somehwere. fixing it now
    Signed-off-by: Jeff Brimager <JBrimager@gmail.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,7 +1,7 @@
 $pkg_name="national-parks"
 $pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 $pkg_origin="winhab"
-$pkg_version="0.1.0"
+$pkg_version="0.1.1"
 $pkg_maintainer="Jeff Brimager <jbrimager@chef.io>"
 $pkg_license=@('Apache-2.0')
 $pkg_deps=@("winhab/tomcat8", "core/corretto8", "winhab/mongodb")
@@ -16,12 +16,13 @@ function Invoke-Prepare {
   # $env:MAVEN_HOME = "$(hab pkg path winhab/maven)"
 }
 function Invoke-Build {
-  Copy-Item "$PLAN_CONTEXT/../" "$HAB_CACHE_SRC_PATH/$pkg_dirname\" -Recurse -Force -Verbose
-  cd "$HAB_CACHE_SRC_PATH\${pkg_dirname}\$pkg_name\"
+  Copy-Item "$PLAN_CONTEXT/../" "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Recurse -Force -Verbose
+  cd "$HAB_CACHE_SRC_PATH\${pkg_dirname}\$pkg_name"
   mvn package
 }
 
 function Invoke-Install{
-  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\$pkg_name\target\$pkg_name.war" "${Prefix}" -verbose
-  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\$pkg_name\data\national-parks.json" "${PREFIX}" -verbose
+  New-Item -ItemType Directory -Path "${pkg_prefix}/tc"
+  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\$pkg_name\target\$pkg_name.war" "$pkg_prefix\" -verbose
+  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\$pkg_name\data\national-parks.json" "$pkg_prefix\" -verbose
 }
